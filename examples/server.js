00c456b498aca2b9a0118145cdeb8658837f6ce1
binhong <544289495@qq.com>
2019-09-11 17:04:47 +0800

    feat: handle response

--- examples/server.js
+++ examples/server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser')
 const webpack = require('webpack')
 const webpackDevMiddleware = require('webpack-dev-middleware')
 const webpackHotMiddleware = require('webpack-hot-middleware')
-const WebpackConfig = require('./examples/simple/webpack.config')
+const WebpackConfig = require('./webpack.config')
 
 const app = express()
 const router = express.Router()
@@ -30,6 +30,26 @@ router.get('/simple/get', function (req, res) {
   })
 })
 
+router.get('/base/get', function (req, res) {
+  res.json(req.query)
+})
+
+router.post('/base/post', function (req, res) {
+  res.json(req.body)
+})
+
+router.post('/base/buffer', function (req, res) {
+  let msg = [];
+  req.on('data', (chunk) => {
+    if (chunk) {
+      msg.push(chunk)
+    }
+  })
+  req.on('end', () => {
+    let buf = Buffer.concat(msg)
+    res.json(buf.toJSON())
+  })
+})
 app.use(router)
 
 const port = process.env.PORT || 3000
